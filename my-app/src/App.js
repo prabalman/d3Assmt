956e297b1da9a79a6d548a75c5c90826555e369f
prabalman <prabalman@gmail.com>
2021-12-04 22:22:12 -0500

    First commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'c3/c3.css';
+
+import React from "react";
+import { Chart } from "./Chart";
+
+function App() {
+  return <Chart />;
+ 
+}
+
+export default App;
